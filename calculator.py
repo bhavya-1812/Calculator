88802f7eef74c2b0bf5cb7f1f908c625ad308923
bhavya-1812 <nomulabhavya181@gmail.com>
2024-06-15 20:25:36 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,58 @@
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def multi(a, b):
+    return a * b
+
+
+def div(a, b):
+    return a/ b
+
+
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Sub")
+print("3.Multi")
+print("4.Div")
+
+
+while True:
+    
+    choice = input("Enter choice(1/2/3/4/): ")
+
+
+    if choice in ('1', '2', '3', '4', ):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", sub(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multi(num1, num2))
+                  
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", div(num1, num2))
+        
+             
+            
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
